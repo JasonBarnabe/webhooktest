fa8ff095fbe82d7150a78ed404347d59551a219c
Jason Barnabe <jason.barnabe@gmail.com>
2014-06-08 22:57:12 -0500

    commit 2

--- test2.user.js
+++ test2.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name something else 9
+// @name something else 10
 // @namespace http://userstyles.org/automation
 // @description Disables things like alert, confirm, and prompt - useful for visiting sites with an automated tool like iMacros
 // @include http://*
