9083ca33c19cb9c1dbbfd2462abd658cc788709d
Jason Barnabe <jason.barnabe@gmail.com>
2014-06-08 22:09:11 -0500

    Create test2.user.js

---
+++ test2.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name something else
+// @namespace http://userstyles.org/automation
+// @description Disables things like alert, confirm, and prompt - useful for visiting sites with an automated tool like iMacros
+// @include http://*
+// @include https://*
+// @version 1
+// ==/UserScript==
+function fixIt() {
+window.onbeforeunload = function() {};
+}
+fixIt();
+setInterval(fixIt, 200);
+window.alert = function() {};
+window.confirm = function() {};
+window.prompt = function() {};
