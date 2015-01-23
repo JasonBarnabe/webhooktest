7cf8f518f83b74daaeed084b8886de4b04044f63
Jason Barnabe <jason.barnabe@gmail.com>
2015-01-23 12:22:38 -0600

    Add file with space

---
+++ file with space.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name testd
+// @name:fr le testd
+// @name:de die testd
+// @namespace   http://userstyles.org/automation
+// @description Disables things like alert, confirm, and prompt - useful for visiting sites with an automated tool like iMacros
+// @description:fr le disable
+// @description:de die disable
+// @include     http://*
+// @include     https://*
+// @version     3
+// ==/UserScript==
+function fixIt() {
+	window.onbeforeunload = function() {};
+}
+fixIt();
+setInterval(fixIt, 200);
+window.alert = function() {};
+window.confirm = function() {};
+window.prompt = function() {};
